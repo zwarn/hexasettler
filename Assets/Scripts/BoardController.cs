06815170325feebba39af200029c70ca7f93b226
zwarn <Michael.Budahn@gmail.com>
2018-07-18 22:08:12 +0200

    parameterize Grid

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -6,6 +6,8 @@ public class BoardController : MonoBehaviour {
     public static BoardController instance;
 
     public int size = 5;
+    public float deltaX = 1.735f;
+    public float deltaY = 1.5f;
     public TileMap tileMap;
 
     void Awake() {
@@ -18,4 +20,9 @@ public class BoardController : MonoBehaviour {
     void Start () {
         tileMap = new TileMap(size, size);
 	}
+
+    public Vector3 coordToVector3(int x, int y)
+    {
+        return new Vector3(x * deltaX + (y % 2 == 1 ? 0f : deltaX/2), y * deltaY);
+    }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,18 +9,19 @@ public class Spawner : MonoBehaviour {
     public GameObject tile;
     public GameObject map;
 
+    private BoardController boardController;
+
     public GameObject spawnTile(int x, int y)
     {
-        Vector3 position = coordToVector3(x, y);
+        Vector3 position = boardController.coordToVector3(x, y);
         GameObject newTile = GameObject.Instantiate(tile, position, Quaternion.identity, map.transform);
         newTile.name = x + " : " + y;
         return newTile;
     }
 
-    public Vector3 coordToVector3(int x, int y)
+    void Start()
     {
-        //TODO: parameterize
-        return new Vector3(x * 0.5f + (y % 2 == 1 ? 0f : 0.25f), y * 0.435f);
+        boardController = BoardController.instance;
     }
 
     void Awake()
