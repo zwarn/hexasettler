0fb20a7aa3c822f24fe66323fd2dbe5194676cc1
zwarn <Michael.Budahn@gmail.com>
2018-07-21 13:21:40 +0200

    make camera scrollable

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    float scrollSpeed = 4f;
+    
+	void Update () {
+        Vector3 delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * scrollSpeed;
+        this.GetComponent<Camera>().transform.Translate(delta);
+	}
+}
