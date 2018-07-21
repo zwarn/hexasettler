8219f8a66ed4eb90f7acce2a3c46aef2d9e43359
zwarn <Michael.Budahn@gmail.com>
2018-07-21 13:02:01 +0200

    make highlightable

---
+++ Assets/Scripts/view/Highlightable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlightable : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(0.9f, 0.9f, 0.9f);
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f);
+    }
+
+}
