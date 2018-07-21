317ebe2e7839167f16aeb626a3b2777c49f7836a
zwarn <Michael.Budahn@gmail.com>
2018-07-21 22:12:09 +0200

    mouse wheel zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     float scrollSpeed = 4f;
+    float zoomSpeed = 100f;
     
 	void Update () {
+        Camera camera = this.GetComponent<Camera>();
+
         Vector3 delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * scrollSpeed;
-        this.GetComponent<Camera>().transform.Translate(delta);
-	}
+        camera.transform.Translate(delta);
+
+        float deltaZoom = Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomSpeed * -1;
+        camera.orthographicSize = Mathf.Clamp(camera.orthographicSize + deltaZoom, 2f, 50f);
+
+    }
 }
