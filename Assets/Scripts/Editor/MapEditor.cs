2bfa86b9de48c568490c5cf30b8c25494577d221
zwarn <Michael.Budahn@gmail.com>
2018-10-04 08:23:31 +0200

    Editor spawns tiles

--- Assets/Scripts/Editor/MapEditor.cs
+++ Assets/Scripts/Editor/MapEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,15 +11,11 @@ namespace Editor
         private int _layerSelection;
         private string[] _layerOptions = {"nothing", "terrain", "objects"};
         private int _terrainSelection;
-        private string[] _terrainOptions = {"gras", "dirt"};
         private static GameObject[] _terrains;
         
         private void OnSceneGUI()
-        {
-            GameObject map =
-                Selection.gameObjects.FirstOrDefault(gameObject => gameObject.GetComponent<BoardController>() != null);
-            
-            if (map != null && _layerSelection > 0)
+        {   
+            if (_layerSelection > 0)
             {
                 int controlId = GUIUtility.GetControlID(FocusType.Passive);
                 Event e = Event.current;
@@ -27,8 +24,16 @@ namespace Editor
                 switch (e.GetTypeForControl(controlId))
                 {
                     case EventType.MouseDown:
-                        Debug.Log("click at " + e.mousePosition + " with " + (e.button == 0 ? "left" : "right"));
-                        e.Use();
+                        Debug.Log(e.button);
+                        if (e.button == 0)
+                        {
+                            Grid grid = ((BoardController) target).GetComponent<Grid>();
+                            var clickWorldPosition = HandleUtility.GUIPointToWorldRay (e.mousePosition).origin;
+                            Vector3Int clickCellPosition = grid.WorldToCell(clickWorldPosition);
+                            Vector3 cellCenterWorld = grid.GetCellCenterWorld(clickCellPosition);
+                            spawmObject(cellCenterWorld, _terrains[_terrainSelection]);
+                            e.Use();
+                        }
                         break;
                 }
             }
@@ -36,8 +41,8 @@ namespace Editor
         
         public override void OnInspectorGUI()
         {
-            //TODO: auslagern
             base.OnInspectorGUI();
+            //TODO: auslagern do once
             _terrains = Resources.LoadAll<GameObject>("Terrain");
             _layerSelection = GUILayout.SelectionGrid(_layerSelection, _layerOptions, 3);
             _terrainSelection = GUILayout.SelectionGrid(_terrainSelection, extractTexture(_terrains), 5);
@@ -47,6 +52,14 @@ namespace Editor
         {
             return gameObjects.Select(o => o.GetComponent<SpriteRenderer>()).Where(renderer => renderer != null)
                 .Select(renderer => renderer.sprite.texture).ToArray();
+        }
+
+        private void spawmObject(Vector3 position, GameObject objectToSpawn)
+        {
+            //TODO: spawn under child
+            //TODO: undo, redo
+//            GameObject map = ((BoardController) target).gameObject;
+            Instantiate(objectToSpawn, position, Quaternion.identity);
         }
     }
 }
